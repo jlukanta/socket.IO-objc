e9e76564e0a348c9e53f415876be5d484d3cbd6f
James Lukanta <james.lukanta@gmail.com>
2014-03-15 12:23:41 -0700

    master: Fixed type error from NSDictionary to NSMutableDictionary

--- SocketIO.m
+++ SocketIO.m
@@ -622,7 +622,7 @@ NSString* const SocketIOException = @"SocketIOException";
     _isConnecting = NO;
     
     if ([_delegate respondsToSelector:@selector(socketIO:onError:)]) {
-        NSMutableDictionary *errorInfo = [NSDictionary dictionaryWithObject:error.localizedDescription forKey:NSLocalizedDescriptionKey];
+        NSMutableDictionary *errorInfo = [NSMutableDictionary dictionaryWithObject:error.localizedDescription forKey:NSLocalizedDescriptionKey];
         
         NSError *err = [NSError errorWithDomain:SocketIOError
                                            code:SocketIOHandshakeFailed
